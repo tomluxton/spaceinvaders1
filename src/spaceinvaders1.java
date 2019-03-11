ed38580b6c9955f8630cc94b3d8441fbd76da067
tom <ta.luxton@gmail.com>
2019-03-11 15:14:43 +1000

    Updated game so that enemy sprites exist and shoot. Need to fix timing of shooting.

--- src/spaceinvaders1.java
+++ src/spaceinvaders1.java
@@ -1,5 +1,6 @@
 package com.almasb.invaders;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,9 +9,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class spaceinvaders1 extends Application{
     private Pane root = new Pane();
+
+    private double t = 0;
 
     private Sprite player = new Sprite(300,750,40,40, "player", Color.BLUE);
 
@@ -19,10 +25,76 @@ public class spaceinvaders1 extends Application{
 
         root.getChildren().add(player);
 
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+            }
+        };
+        timer.start();
+
+        nextLevel();
+
         return root;
     }
 
-    private void shoot(){
+
+    private void nextLevel(){
+        for(int i = 0; i< 5; i++){
+            Sprite s = new Sprite(90 + i*100, 150, 30, 30, "enemy", Color.RED);
+
+            root.getChildren().add(s);
+
+        }
+    }
+
+    private List<Sprite> sprites(){
+        return root.getChildren().stream().map(n-> (Sprite)n).collect(Collectors.toList());
+    }
+
+    private  void update(){
+        t+=0.016;
+
+        sprites().forEach(s-> {
+            switch (s.type){
+                case "enemybullet":
+                    s.moveDown();
+                    if(s.getBoundsInParent().intersects(player.getBoundsInParent())){
+                        player.dead = true;
+                        s.dead = true;
+                    }
+                    break;
+                case "playerbullet":
+                    s.moveUp();
+
+                    sprites().stream().filter(e -> e.type.equals("enemy")).forEach(enemy -> {
+                        if (s.getBoundsInParent().intersects(enemy.getBoundsInParent())){
+                            enemy.dead = true;
+                            s.dead = true;
+                        }
+                    });
+                    break;
+
+                case "enemy":
+                    if (t>2){
+                        if(Math.random()< 0.3){
+                            shoot(s);
+                        }
+                    }
+                    break;
+            }
+        });
+        root.getChildren().removeIf(n-> {
+            Sprite s = (Sprite) n;
+            return s.dead;
+        });
+
+    }
+
+    private void shoot(Sprite who){
+        Sprite s = new Sprite((int) who.getTranslateX()+ 20, (int) who.getTranslateY(),5, 20, who.type+"bullet", Color.BLACK);
+
+        root.getChildren().add(s);
 
     }
 
@@ -40,7 +112,7 @@ public class spaceinvaders1 extends Application{
                     player.moveRight();
                     break;
                 case SPACE:
-                    shoot();
+                    shoot(player);
                     break;
             }
         });
